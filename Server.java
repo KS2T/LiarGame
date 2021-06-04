7df1268bade96dbbdfd92ef55303e5a7f4476dae
braveleftji <braveleftji@daum.net>
2021-06-04 16:53:09 +0900

    gi

---
+++ Server.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+class Server {
+  Object o;
+  String ip,port,id;
+
+  LoginUi ui;
+  Server(LoginUi ui, JFrame frame){
+    this.ui = ui;
+  }
+
+}
